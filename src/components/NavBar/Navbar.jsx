00fe50580225a0aa361cfd62d6027436330373e6
Mohammed Ali <mohammedali314>
2025-04-28 13:03:54 +0500

    Fix import path for navbar styles in Navbar component for consistency.

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import '../Navbar/navbar.scss'
+import './navbar.scss'
 import { AiFillCloseCircle } from 'react-icons/ai'
 import { TbGridDots } from 'react-icons/tb'
 import logo from '../../Assets/LOGOO.png'
